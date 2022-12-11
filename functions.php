ba4062183fe67493dc8c56480d42aa945001c15d
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-12-11 12:14:13 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function hospitalFunctions() {
+
+
+    wp_enqueue_style('hospital_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('hospital_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+add_action('wp_enqueue_scripts','hospitalFunctions');
+
+
+function hospital_features(){
+
+    register_nav_menu('headerMenuLocation','Header Menu Location');
+    register_nav_menu('footerLocationOne','Footer Location One');
+    register_nav_menu('footerLocationTwo','Footer Location Two');
+}
+add_action('after_setup_theme', 'hospital_features');
---
+++ page.php
@@ -0,0 +1,70 @@
+<?php 
+ get_header();
+ ?>
+
+
+ <div class="page-banner">
+ <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+ <div class="page-banner__content container container--narrow">
+   <h1 class="page-banner__title"><?php the_title(); ?></h1>
+   <div class="page-banner__intro">
+     <p>Welcome to our Blog.</p>
+   </div>
+ </div>
+</div>
+
+<div class="container container--narrow page-section">
+
+<?php 
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if($theParent){ ?>
+
+        <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>">
+          <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?>
+          </a> <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
+
+    <?php }
+ ?>
+
+
+
+ <?php
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
+    if($testArray or $theParent){?>  
+ 
+ <div class="page-links">
+   <h2 class="page-links__title"><a href="<?php the_permalink($theParent); ?>"><?php echo get_the_title($theParent);?></a></h2>
+   <ul class="min-list">
+   <?php 
+    if($theParent){
+        $findChildOf = $theParent;
+    }else{
+        $findChildOf = get_the_ID();
+    }
+    wp_list_pages(array(
+        'title_li' => Null,
+        'child_of' => $findChildOf,
+        'sort_column'=> 'menu_order'
+    ));
+ ?>
+   </ul>
+   <?php }
+   ?>
+ </div>
+
+ <div class="generic-content">
+   <?php the_content(); ?>
+</div>
+</div>
+
+
+
+<?php
+get_footer();
+?>
