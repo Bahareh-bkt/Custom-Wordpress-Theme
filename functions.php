5deb66fb7adb9991d194f96e37b63c63e73ee0cd
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-12-12 14:41:36 +0100

    Making new Post Type (Event)

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function hospitalFunctions() {
+function hospital_functions() {
 
 
     wp_enqueue_style('hospital_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('hospital_extra_styles', get_theme_file_uri('/build/index.css'));
 }
-add_action('wp_enqueue_scripts','hospitalFunctions');
+add_action('wp_enqueue_scripts','hospital_functions');
 
 
 function hospital_features(){
@@ -16,3 +16,4 @@ function hospital_features(){
     register_nav_menu('footerLocationTwo','Footer Location Two');
 }
 add_action('after_setup_theme', 'hospital_features');
+
