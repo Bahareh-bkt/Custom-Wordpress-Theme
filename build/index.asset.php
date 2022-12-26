8d1773958374fdd9b515735c6e63278b6515a910
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-12-26 13:53:57 +0100

    define openButton

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '24b788c7662da9b721e5');
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,34 +1,41 @@
-import $ from "Jquery"
+import $ from "jquery"
 
 class Search {
 
     constructor(){
         this.addSearchHTML()
-        
+        this.openButton = $(".js-search-trigger")
+        this.searchOverlay = $(".search-overlay")
+        this.isOverlayOpen = false
+        this.events()
 
     }
 
     events(){
+        this.openButton.on("click", this.openOverlay.bind(this))
+    }
 
+    openOverlay(){
+        this.searchOverlay.addClass("search-overlay--active")
+        $("body").addClass("body-no-scroll")
+        this.isOverlayOpen = true
     }
 
     addSearchHTML(){
         $("body").append(`
-           <div class="search-overlay">
-            <div class="search-overlay__top">
-                <div class="container">
-                    <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                    <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
-                    <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
-                </div>
-            </div>
+        <div class="search-overlay">
+          <div class="search-overlay__top">
             <div class="container">
-                <div id="search-overlay__results"></div>
+              <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+              <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+              <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
             </div>
-           </div> 
-        `)
-        
-    }
+          </div>
+          <div class="container">
+            <div id="search-overlay__results"></div>
+          </div>
+      </div>`)
+      }
 }
 
 export default Search
