f9a3a3b7b586719cd69adb773b804de817a7d9f1
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-12-26 17:46:23 +0100

    add index.js.map

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 
 function hospital_functions() {
-    wp_enqueue_style('hospital_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('hospital_extra_styles', get_theme_file_uri('/build/index.css'));
-    wp_enqueue_script('main_js', get_theme_file_uri('build/index.js') , array('jquery') , 1.0 , true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true); /*Slideshow behavior*/
+     wp_localize_script( 'main-university-js', 'universityData', array(
+      'root_url' => get_site_url()
+     ) );
+     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');  /*social media icons*/
+     wp_enqueue_style('custome-google-font', 
+     '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 add_action('wp_enqueue_scripts','hospital_functions');
 
